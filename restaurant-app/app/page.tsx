ee683053c0b9c4a3ba586816d5302abd2662d3d6
davli <davli@davli.myguest.virtualbox.org>
2026-06-08 17:59:30 +0200

    navbar;

--- restaurant-app/app/page.tsx
+++ restaurant-app/app/page.tsx
@@ -3,11 +3,15 @@ export default function Home() {
     <main>
       <div id="navbar">
         <img src="/SushiHouse.png"/>
-        <p>Accueil</p>
-        <p>Menu</p>
-        <p>Commander</p>
-        <p>Connexion</p>
-        <p>Panier</p>
+        <div id="navbar-btn">
+          <p>Accueil</p>
+          <p>Menu</p>
+          <p>A propos</p>
+        </div>
+        <div id="navbar-link">
+          <p>Compte <i className="fa-solid fa-user"></i></p>
+          <p>Panier <i className="fa-solid fa-bag-shopping"></i><span className="cart-badge">0</span></p>
+        </div>
       </div>
     </main>
   );
