f64127f7d7ce45d004bf320987e19675aab5e649
davli <davli@davli.myguest.virtualbox.org>
2026-06-22 14:17:23 +0200

    click menu

--- restaurant-app/components/Menu.tsx
+++ restaurant-app/components/Menu.tsx
@@ -1,23 +1,29 @@
+"use client"
+
 import { menu } from "../data/menu";
+import { useState } from "react";
 
 export default function Menu() {
+  const [open, setOpen] = useState(false);
+
   return (
     <div className="relative inline-block group">
-      <button className="rounded text-white hover:bg-blue-700">
+      <button onClick={() => setOpen(!open)} className="text-white hover:text-pink-100">
         Menu
       </button>
 
-      <div className="absolute left-0 top-full hidden w-48 rounded border bg-white shadow-lg group-hover:block">
+      {open && (<div className="absolute left-0 top-full hidden w-48 rounded border bg-white shadow-lg group-hover:block">
         {menu.map((category) => (
           <a
             key={category.category}
             href={`#${category.category}`}
             className="block px-4 py-2 hover:bg-gray-100"
+            onClick={() => setOpen(false)}
           >
             {category.category}
           </a>
         ))}
-      </div>
+      </div>)}
     </div>
   );
 }
