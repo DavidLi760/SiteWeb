a047e7b03b2e39f98c98693da76562ff7001619f
davli <davli@davli.myguest.virtualbox.org>
2026-06-23 14:14:29 +0200

    about

---
+++ restaurant-app/app/about/page.tsx
@@ -0,0 +1,32 @@
+import Navbar from "../.././components/Navbar";
+import Footer from "../.././components/Footer"
+
+export default function Home() {
+    return (
+    <main>
+        <Navbar />
+        <div className="w-full bg-white shadow-lg rounded-xl p-8 text-center">
+        
+        <h1 className="text-3xl font-bold mb-4">
+          À propos de Sushi House 🍣
+        </h1>
+
+        <p className="text-gray-600 mb-4">
+          Bienvenue chez Sushi House, un restaurant passionné par la cuisine japonaise authentique.
+          Nous préparons chaque plat avec des ingrédients frais et de qualité.
+        </p>
+
+        <p className="text-gray-600 mb-4">
+          Notre mission est de vous offrir une expérience culinaire simple, rapide et délicieuse,
+          directement inspirée du Japon.
+        </p>
+
+        <p className="text-gray-600">
+          Merci de votre visite et bon appétit ! 🍱
+        </p>
+
+      </div>
+        <Footer />
+    </main>
+    );
+}
--- restaurant-app/components/Navbar.tsx
+++ restaurant-app/components/Navbar.tsx
@@ -1,30 +1,41 @@
+"use client"
+
+import Link from "next/link"
 import Menu from "./Menu"
+import { usePathname } from "next/navigation";
 
 export default function Navbar() {
+  const pathname = usePathname();
   return (
     <div className="flex flex-row justify-between items-center sm:h-[100px] h-[60px] px-4 bg-[#716969]">
       
-      <img src="/SushiHouse.png" className="h-full" />
+      <Link href="/" className="flex items-center h-full">
+        <img src="/SushiHouse.png" className="h-full" />
+      </Link>
 
       <div className="flex gap-12">
-        <p className="hidden sm:flex">Accueil</p>
-        <Menu />
-        <p className="hidden sm:flex ">A propos</p>
+        <Link className="hidden sm:flex text-white" href="/">Accueil</Link>
+        {pathname === "/" && <Menu />}
+        <Link className="hidden sm:flex text-white" href="/about">A propos</Link>
       </div>
 
       <div className="flex gap-12 relative">
-        <p>
-          Compte <i className="fa-solid fa-user text-2xl"></i>
-        </p>
+        <div className="flex">  
+          <p className="hidden sm:block">
+            Compte
+          </p>
+          <i className="fa-solid fa-user text-2xl"></i>
+        </div>
 
-        <p className="relative">
-          Panier{" "}
+        <div className="flex">
+          <p className="relative hidden sm:block">
+            Panier{" "}
+          </p>
           <i className="fa-solid fa-bag-shopping text-2xl"></i>
-
           <span className="absolute -top-2 -right-2 bg-red-600 text-white text-xs w-5 h-5 flex items-center justify-center rounded-full">
             0
           </span>
-        </p>
+        </div>
       </div>
 
     </div>
