6831d33ea996c63898951efbbef38ccaa7ab3b8d
davli <davli@davli.myguest.virtualbox.org>
2026-07-08 15:02:29 +0200

    md

--- restaurant-app/components/Category.tsx
+++ restaurant-app/components/Category.tsx
@@ -9,7 +9,7 @@ export default function Category({ title, id, children }: CategoryProps) {
     <section id={id} className="mt-8 scroll-mt-24 bg-gradient-to-r from-[#FFFBFB] to-[#FFFFFF]">
       <h2 className="text-5xl font-bold mb-4 text-center">{title}</h2>
 
-      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 gap-4 justify-items-center">
+      <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4 justify-items-center">
         {children}
       </div>
     </section>
