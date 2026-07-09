3f9b9f5a8a1a19cfffec37d6b930ccaa42035209
davli <davli@davli.myguest.virtualbox.org>
2026-07-09 15:06:40 +0200

    global.css cleared

--- restaurant-app/app/page.tsx
+++ restaurant-app/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main>
       <Navbar />
-      <img id="lobby-img" src="/lobby.png"/>
+      <img id="lobby-img" src="/lobby.png" className="w-full h-40 sm:h-60 md:h-80 object-cover"/>
       <div className="flex gap-4 justify-center bg-gradient-to-r from-[#FFFBFB] to-[#FFFFFF] p-4">
         <button className="px-6 sm:px-16 py-3 border border-black bg-gradient-to-b from-[#68b000] to-[#70e000] text-black rounded">
           Emporter
