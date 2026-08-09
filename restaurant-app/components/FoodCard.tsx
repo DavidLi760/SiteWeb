d4a3b47ab7f22f1fc0c99be024e1241dd9cbee55
David <linebulios2003@gmail.com>
2026-08-09 14:19:11 +0200

    responsive foodcard

--- restaurant-app/components/FoodCard.tsx
+++ restaurant-app/components/FoodCard.tsx
@@ -6,7 +6,7 @@ type FoodCardProps = {
 
 export default function FoodCard({ name, price, image }: FoodCardProps) {
   return (
-    <div className="mt-4 p-4 cursor-pointer bg-white shadow-lg hover:shadow-xl rounded w-40 sm:w-60">
+    <div className="mt-4 p-4 aspect-[3/2] cursor-pointer bg-white shadow-lg hover:shadow-xl rounded w-full">
         <img src={image} className="w-full h-30 sm:h-40 object-cover rounded"/>
         <p className="font-bold truncate">{name}</p>
         <p className="text-gray-600">{price} €</p>
